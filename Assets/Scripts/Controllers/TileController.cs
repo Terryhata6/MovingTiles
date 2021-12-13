16ba654180df00fe769b73aa3a4400989aab473f
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-12-13 17:54:31 +0300

    Commmit

--- Assets/Scripts/Controllers/TileController.cs
+++ Assets/Scripts/Controllers/TileController.cs
@@ -1,9 +1,40 @@
+using System;
+using Core.Entities;
 using UnityEngine;
+using UnityEngine.WSA;
 
 namespace Core
 {
     public class TileController : MonoBehaviour
     {
-        
+        [SerializeField] private TileBox tileBoxExample;
+        [SerializeField] private int _dimension;
+        [SerializeField] private float _step;
+        private TileBox tempTileBox;
+        public void Awake()
+        {
+            CreateTiles();
+        }
+
+        public void CreateTiles()
+        {
+            for (int i = -_dimension; i <= _dimension; i++)
+            {
+                for (int j = -_dimension; j <=_dimension; j++)
+                {
+                    tempTileBox = Instantiate(tileBoxExample.gameObject,
+                        transform.position + Vector3.right * (i * _step) + Vector3.forward * (j * _step),
+                        Quaternion.identity, transform).GetComponent<TileBox>();
+                    if (Mathf.Abs(i) > Mathf.Abs(j))
+                    {
+                        tempTileBox.SetDistance(Mathf.Abs(i));
+                    }
+                    else
+                    {
+                        tempTileBox.SetDistance(Mathf.Abs(j));
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Entities/TileBox.cs
+++ Assets/Scripts/Entities/TileBox.cs
@@ -4,6 +4,11 @@ namespace Core.Entities
 {
     public class TileBox : MonoBehaviour
     {
-        
+        [SerializeField] private int _distanceFromCenter;
+
+        public void SetDistance(int distance)
+        {
+            _distanceFromCenter = distance * 10;
+        }
     }
 }
