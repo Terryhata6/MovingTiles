4e0f41c8de9f20b7d90f5166b30515b8ce05ad31
ENTER-ALT <rodustish88@mail.ru>
2021-12-17 00:22:04 +0300

    Commit

--- Assets/Scripts/Utilits/PlayerSkillPointer.cs
+++ Assets/Scripts/Utilits/PlayerSkillPointer.cs
@@ -1,80 +1,97 @@
+using System;
+using System.Collections;
 using Core.Entities;
 using UnityEngine;
 
 namespace Core.UtilitsSpace
 {
-    public class PlayerSkillPointer: MonoBehaviour
+    public class PlayerSkillPointer
     {
-        [SerializeField] private GameObject _playerSkillInstance;
         private Vector3 _tempPos;
         private RaycastHit _hit;
         private Collider _lastGameObj;
-        
-        
+        private bool _skillCanSnap;
+        private bool _pointSkill = false;
+        private TileBox _tile;
 
-        private void FixedUpdate()
+
+
+        public IEnumerator PointSkill(TilableObject _playerSkill, Action onEndSpawningCallback) //eNTER-ALT
         {
-            if (_playerSkillInstance.Equals(null))
+            _pointSkill = true;
+            while (_pointSkill)
             {
-                return;
-            }
-
-            _tempPos = InputController.Instance.TouchPosition(out _hit, ~(1<<7));
-            if (!_hit.Equals(null))
-            {
-                if (_hit.collider.gameObject.layer.Equals(6) )
+                _tempPos = InputController.Instance.TouchPosition(out _hit, ~(1<<7));
+                if (!_hit.Equals(null))
                 {
-                    if (_hit.collider.Equals(_lastGameObj))
+                    if (_hit.collider.gameObject.layer.Equals(6) )
                     {
+                        if (_hit.collider.Equals(_lastGameObj))
+                        {
+                        }
+                        else
+                        {
+                            _tile = _hit.collider.gameObject.GetComponent<TileBox>();
+                            CheckTile(_tile);
+                            _tempPos = _hit.collider.transform.position;
+                            _lastGameObj = _hit.collider;
+                            _playerSkill.transform.position = _tempPos;
+                        }
+                    
                     }
                     else
                     {
-                        CheckTile(_hit.collider.gameObject.GetComponent<TileBox>());
-                        _tempPos = _hit.collider.transform.position;
-                        _lastGameObj = _hit.collider;
-                        _playerSkillInstance.transform.position = _tempPos;
+                        _skillCanSnap = false;
+                        _tempPos.y = 1f;
+                        _playerSkill.transform.position = _tempPos;
                     }
-                    
+
                 }
                 else
                 {
                     _tempPos.y = 1f;
-                    _playerSkillInstance.transform.position = _tempPos;
+                    _playerSkill.transform.position = _tempPos;
                 }
-
+                yield return null;
+            }
+            if (_skillCanSnap)
+            {
+                _playerSkill.SetBox(_tile);
+                _playerSkill.StartCoroutine(_playerSkill.SpawnAnimation((value) =>
+                {
+                    TilableObjectsController.Instance.AddObjectToList(value as TilableObject);
+                }));
             }
             else
             {
-                _tempPos.y = 1f;
-                _playerSkillInstance.transform.position = _tempPos;
+                GameObject.Destroy(_playerSkill);
             }
-
-            
+            onEndSpawningCallback?.Invoke();
         }
 
         public void CheckTile(TileBox tile)
         {
-            // if (tile)
-            // {
-            //     if (tile!.TileBusy)
-            //     {
-            //         Debug.Log("свободно");
-            //     }
-            //     else
-            //     {
-            //         Debug.Log("несвободно");
-            //     }
-            // }
+            if (tile)
+            {
+                if (tile!.TileBusy)
+                {
+                    _skillCanSnap = true;
+                    Debug.Log("свободно");
+                }
+                else
+                {
+                    _skillCanSnap = false;
+                    Debug.Log("несвободно");
+                }
+            }
         }
         
-        public void SetSkill(GameObject skill)
-        {
-            _playerSkillInstance = skill;
-        }
 
         public void DropSkill()
         {
-            _playerSkillInstance = null;
+            _pointSkill = false;
         }
+        
+        
     }
 }
